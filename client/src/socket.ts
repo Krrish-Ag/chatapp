1d17de3c44c6fc49b8407f0da89dc0064ce63beb
KRRISH AGARWAL <krrishapj@gmail.com>
2025-10-05 14:38:25 +0530

    updated backend url

--- client/src/socket.ts
+++ client/src/socket.ts
@@ -1,5 +1,6 @@
 import { io } from "socket.io-client";
 
-const url = "http://localhost:3000";
+// const url = "http://localhost:3000";
+const url = import.meta.env.VITE_RENDER_BACKEND_URL;
 
 export const socket = io(url);
